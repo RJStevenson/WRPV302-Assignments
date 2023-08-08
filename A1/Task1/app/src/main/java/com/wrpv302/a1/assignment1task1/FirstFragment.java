59143d921b2a3c88cbabb2172c1d2c175cc15407
James-Steve <hamsterofgold@gmail.com>
2023-08-08 20:08:40 +0200

    A1Task1 Almost done

--- A1/Task1/app/src/main/java/com/wrpv302/a1/assignment1task1/FirstFragment.java
+++ A1/Task1/app/src/main/java/com/wrpv302/a1/assignment1task1/FirstFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -11,9 +13,14 @@ import androidx.navigation.fragment.NavHostFragment;
 
 import com.wrpv302.a1.assignment1task1.databinding.FragmentFirstBinding;
 
+import org.w3c.dom.Text;
+
+import java.util.Random;
+
 public class FirstFragment extends Fragment {
 
     private FragmentFirstBinding binding;
+    private int i = 0;
 
     @Override
     public View onCreateView(
@@ -28,7 +35,32 @@ public class FirstFragment extends Fragment {
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        view.findViewById(R.id.btnrandom).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            TextView TV = (TextView) view.getRootView().findViewById(R.id.txts);
+                TV.setText("Your Random number is: " +new Random().ints(0,100).findFirst().getAsInt());
+            }
+        });
 
+        binding.btncount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+              TextView TV=  (TextView) view.getRootView().findViewById(R.id.txtvm);
+              i++;
+              TV.setText(Integer.toString(i));
+            }
+            }
+        );
+
+        binding.btntoast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast myToast = Toast.makeText(getActivity(), "Hello toast!", Toast.LENGTH_SHORT);
+                myToast.show();
+            }
+
+        });
     }
 
     @Override
