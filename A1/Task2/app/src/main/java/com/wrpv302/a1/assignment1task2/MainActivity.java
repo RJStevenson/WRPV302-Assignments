3025556a73f0f1be67b2c317c3ab8361831343de
James-Steve <hamsterofgold@gmail.com>
2023-08-08 22:40:19 +0200

    A1 Task2 Almost finished gui

---
+++ A1/Task2/app/src/main/java/com/wrpv302/a1/assignment1task2/MainActivity.java
@@ -0,0 +1,14 @@
+package com.wrpv302.a1.assignment1task2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
