e28eb7a119ab83a5035acf5a56d5f2f68759dcfb
James-Steve <hamsterofgold@gmail.com>
2023-08-22 19:02:43 +0200

    A2 Task1 Working

--- A2/Task1/PubSub/src/Main.java
+++ A2/Task1/PubSub/src/Main.java
@@ -1,7 +1,31 @@
 import PubSubBroker.PubSubBroker;
+import PubSubBroker.Subscriber;
+
+import java.nio.channels.Pipe;
 
 public class Main {
+    Integer pine =1;
     public static void main(String[] args) {
         PubSubBroker broker =  PubSubBroker.getInstance();
+
+        Boolean temp = broker.Publish("Banna", 2);
+        temp = broker.RegisterTopic("Score", Integer.class);
+        Main man = new Main();
+        broker.Subscribe("Score",man.onMessageReceived );
+
+        Integer obama = 5;
+        temp = broker.Publish("Score", obama);
+
+        System.out.println(man.pine);
+
     }
+
+
+    public Subscriber onMessageReceived = new Subscriber() {
+        @Override
+        public void OnMessageReceived(Object Data) {
+            pine = (Integer) Data;
+        }
+    };
+
 }
